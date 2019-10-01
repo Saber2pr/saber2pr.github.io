b227fbb11ff2518fce3cb6377716c95964e6f5e2
saber2pr <saber2pr@gmail.com>
2019-10-01 21:41:57 +0000

    feat(activity): add more expand

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.less"
 
 import MD from "@saber2pr/md2jsx"
@@ -66,29 +66,48 @@ const matchText = (type: Act["type"], text: Act["text"]) => {
   return text
 }
 
-export const Activity = ({ acts }: Activity) => (
-  <ul className="Activity">
-    {acts.map(({ type, text, date }, i) => (
-      <li key={text + i}>
-        <dl className="Activity-Content">
-          <dt className="Activity-Type">
-            <strong>{matchType(type)}</strong>
-          </dt>
-          <dd>
-            <ul>
-              <li>
-                <MD theme={md_theme}>{matchText(type, text)}</MD>
-              </li>
-              <li className="Activity-Time">
-                <p>{timeDeltaFromNow(date)}</p>
-              </li>
-            </ul>
-          </dd>
-          <dd>
-            <hr />
-          </dd>
-        </dl>
+export const Activity = ({ acts }: Activity) => {
+  const [length, setLength] = useState(10)
+
+  return (
+    <ul className="Activity">
+      {acts.slice(0, length).map(({ type, text, date }, i) => (
+        <li key={text + i}>
+          <dl className="Activity-Content">
+            <dt className="Activity-Type">
+              <strong>{matchType(type)}</strong>
+            </dt>
+            <dd>
+              <ul>
+                <li>
+                  <MD theme={md_theme}>{matchText(type, text)}</MD>
+                </li>
+                <li className="Activity-Time">
+                  <p>{timeDeltaFromNow(date)}</p>
+                </li>
+              </ul>
+            </dd>
+            <dd>
+              <hr />
+            </dd>
+          </dl>
+        </li>
+      ))}
+      <li>
+        <div
+          style={{
+            textAlign: "center",
+            width: "100%"
+          }}
+        >
+          <span
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+            onClick={() => setLength(length + 5)}
+          >
+            更多
+          </span>
+        </div>
       </li>
-    ))}
-  </ul>
-)
+    </ul>
+  )
+}
