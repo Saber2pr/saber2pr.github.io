c4cb870d5d422932a0d58426a468ee62f37ef7b2
saber2pr <saber2pr@gmail.com>
2019-11-14 20:59:01 +0000

    feat(@saber2pr/memo): update

--- src/request/axios.ts
+++ src/request/axios.ts
@@ -3,4 +3,4 @@ import memo from "@saber2pr/memo"
 
 export const axios = new Request({ timeout: 3600000 })
 
-export const memoGet = memo(axios.get, axios)
+export const memoGet = memo(axios.get, axios, 20)
