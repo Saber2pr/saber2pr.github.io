b82dccb29e3bffb0d8e9bfa2c93ee08ca14fbbd6
saber2pr <saber2pr@gmail.com>
2019-12-12 11:24:13 +0000

    feat(config): clean

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,6 @@
-declare const client_id: string
-declare const client_secret: string
-
 export * from "./browser"
 export * from "./origin"
 import { origin } from "./origin"
-
-origin["client_id"] = client_id
-origin["client_secret"] = client_secret
 
 const Routes = {
   home: {
