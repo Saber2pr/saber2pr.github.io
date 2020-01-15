ba6eb5ac392e941e4141131ab637f63afa04a132
saber2pr <saber2pr@gmail.com>
2020-01-15 12:52:29 +0000

    fix(hook/useBtnDisable): check ref null.

--- src/hooks/useBtnDisable.ts
+++ src/hooks/useBtnDisable.ts
@@ -6,11 +6,13 @@ export const useBtnDisable = (): [
 ] => {
   const ref = useRef<HTMLButtonElement>()
   const disable = () => {
+    if (ref.current === null) return
     ref.current.disabled = true
     ref.current.style.cursor = "not-allowed"
     ref.current.style.opacity = "0.5"
   }
   const enable = () => {
+    if (ref.current === null) return
     ref.current.disabled = false
     ref.current.style.cursor = "pointer"
     ref.current.style.opacity = "1"
