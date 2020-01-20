a67a6b6d6206c4e33da405dd797237f4c554720f
saber2pr <saber2pr@gmail.com>
2020-01-20 13:16:40 +0000

    feat(utils/pwa-installer): add dev

--- src/utils/pwa-installer.ts
+++ src/utils/pwa-installer.ts
@@ -1,5 +1,6 @@
 import { origin } from "../config"
 import { localStore } from "../store"
+import { whenInDEV } from "./whenInDEV"
 
 const { VERSION_KEY, PWA_KEY } = origin.constants
 const WORKER_PATH = origin.workers.pwa
@@ -19,4 +20,7 @@ export const getVersion = () => localStore.getItem(VERSION_KEY)
 
 export const PWAInstaller = async () => {
   await navigator.serviceWorker.register(WORKER_PATH)
+  if (whenInDEV()) {
+    await caches.delete(PWA_KEY)
+  }
 }
