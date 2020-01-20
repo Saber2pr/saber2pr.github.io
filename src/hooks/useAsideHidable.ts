e3d312f4092e565e6e045698f49a03ba1e70774b
saber2pr <saber2pr@gmail.com>
2020-01-20 21:12:53 +0000

    feat(hook/useAsideBtnHidable): init

---
+++ src/hooks/useAsideHidable.ts
@@ -0,0 +1,66 @@
+import { useRef, useState, useEffect } from "react"
+import { useIsMobile } from "./useIsMobile"
+
+const show_ref = { current: true }
+
+export const useAsideHidable = (
+  aside_ref: React.MutableRefObject<HTMLDivElement>
+): [
+  React.MutableRefObject<HTMLDivElement>,
+  React.MutableRefObject<HTMLDivElement>,
+  Function,
+  boolean
+] => {
+  const main_ref = useRef<HTMLDivElement>()
+  const btn_ref = useRef<HTMLDivElement>()
+  const hidden = () => {
+    main_ref.current.style.width = "80%"
+    main_ref.current.style.margin = "0 auto"
+    aside_ref.current.style.width = "0"
+    btn_ref.current.style.right = "0"
+  }
+
+  const show = () => {
+    main_ref.current.style.width = "70%"
+    main_ref.current.style.margin = "0"
+    aside_ref.current.style.width = "30%"
+    btn_ref.current.style.right = "auto"
+  }
+
+  const [isShow, setIsShow] = useState(show_ref.current)
+
+  const select = () => {
+    if (isShow) {
+      hidden()
+      setIsShow(false)
+      show_ref.current = false
+    } else {
+      show()
+      setIsShow(true)
+      show_ref.current = true
+    }
+  }
+
+  useEffect(() => {
+    if (document.documentElement.clientWidth < 760) return
+    show_ref.current ? show() : hidden()
+  }, [])
+
+  useIsMobile(
+    () => {
+      main_ref.current.style.width = "auto"
+      aside_ref.current.style.width = "auto"
+      btn_ref.current.style.display = "none"
+    },
+    () => {
+      btn_ref.current.style.display = "block"
+      if (isShow) {
+        show()
+      } else {
+        hidden()
+      }
+    }
+  )
+
+  return [main_ref, btn_ref, select, isShow]
+}
