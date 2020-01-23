c46f3b0172df15c94ee462c67a68d6217cbabd61
saber2pr <saber2pr@gmail.com>
2020-01-23 20:54:47 +0000

    build(*): update

--- src/utils/pwa-installer.ts
+++ src/utils/pwa-installer.ts
@@ -4,24 +4,24 @@ import { whenInDEV } from "./whenInDEV"
 
 const { VERSION_KEY, PWA_KEY } = origin.constants
 const WORKER_PATH = origin.workers.pwa
-let registration = null
 
 export const freeCache = async () => {
-  await caches.delete(PWA_KEY)
   localStore.clear()
+  await caches.delete(PWA_KEY)
+  const registration = await navigator.serviceWorker.getRegistration()
+  await registration.unregister()
 }
 
 export const updateVersion = async (version: string) => {
-  if (registration) {
-    await registration.update()
-  }
   localStore.setItem(VERSION_KEY, version)
+  const registration = await navigator.serviceWorker.getRegistration()
+  await registration.update()
 }
 
 export const getVersion = () => localStore.getItem(VERSION_KEY)
 
 export const PWAInstaller = async () => {
-  registration = await navigator.serviceWorker.register(WORKER_PATH)
+  await navigator.serviceWorker.register(WORKER_PATH)
   if (whenInDEV()) {
     await caches.delete(PWA_KEY)
   }
