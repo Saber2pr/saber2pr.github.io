16184895d5ffaf9a510c0339a90e21681b5949a7
saber2pr <saber2pr@gmail.com>
2020-04-05 17:50:55 +0000

    build(*): update

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { createState } from "@saber2pr/redux/lib/state"
 export const store = createState({
   context: null,
   blogScrollTop: 0,
-  actLen: 10,
+  actLen: 4,
   actsScrollTop: 0,
   searchLen: 10,
   searchScrollTop: 0
