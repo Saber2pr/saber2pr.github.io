b4860860731eae257058d371ea605efac2062206
saber2pr <saber2pr@gmail.com>
2020-07-12 09:21:50 +0800

    build(*): update

--- scripts/collectUpdates.ts
+++ scripts/collectUpdates.ts
@@ -2,23 +2,23 @@
  * @Author: saber2pr
  * @Date: 2019-09-28 10:54:52
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-10-09 21:46:58
+ * @Last Modified time: 2020-07-12 09:13:59
  */
 import { diff, commit, Commit } from '@saber2pr/git'
 
 export type Update = {
-  type: Commit['type']
-  path: string
-  date: string
+	type: Commit['type']
+	path: string
+	date: string
 }
 
 export const collectUpdates = async (root: string) => {
-  const commits = await diff(root)
-  const updates = commits.map<Update>(({ type, master: { path } }) => ({
-    type,
-    path: path.replace(root, ''),
-    date: new Date().toLocaleString(),
-  }))
-  await commit(commits)
-  return updates
+	const commits = await diff(root)
+	const updates = commits.map<Update>(({ type, master: { path } }) => ({
+		type,
+		path: path.replace(root, ''),
+		date: String(Date.now()),
+	}))
+	await commit(commits)
+	return updates
 }
--- src/utils/timeDelta.ts
+++ src/utils/timeDelta.ts
@@ -1,21 +1,18 @@
-export const timeDelta = (time1: string, time2: string) => {
-  const Time1 = new Date(time1).getTime()
-  const Time2 = new Date(time2).getTime()
+export const timeDelta = (time1: string | number, time2: string | number) => {
+	const seconds = (Number(time1) - Number(time2)) / 1000
+	const minutes = seconds / 60
+	const hours = minutes / 60
+	const days = hours / 24
+	const months = days / 30
+	const years = months / 12
 
-  const seconds = (Time1 - Time2) / 1000
-  const minutes = seconds / 60
-  const hours = minutes / 60
-  const days = hours / 24
-  const months = days / 30
-  const years = months / 12
-
-  if (years >= 1) return `${Math.round(years)} 年前.`
-  if (months >= 1) return `${Math.round(months)} 月前.`
-  if (days >= 1) return `${Math.round(days)} 天前.`
-  if (hours >= 1) return `${Math.round(hours)} 小时前.`
-  if (minutes >= 1) return `${Math.round(minutes)} 分钟前.`
-  if (seconds >= 1) return `${Math.round(minutes)} 秒前.`
+	if (years >= 1) return `${Math.round(years)} 年前.`
+	if (months >= 1) return `${Math.round(months)} 月前.`
+	if (days >= 1) return `${Math.round(days)} 天前.`
+	if (hours >= 1) return `${Math.round(hours)} 小时前.`
+	if (minutes >= 1) return `${Math.round(minutes)} 分钟前.`
+	if (seconds >= 1) return `${Math.round(minutes)} 秒前.`
 }
 
-export const timeDeltaFromNow = (time: string) =>
-  timeDelta(new Date().toUTCString(), time)
+export const timeDeltaFromNow = (time: string | number) =>
+	timeDelta(Date.now(), Number(time))
