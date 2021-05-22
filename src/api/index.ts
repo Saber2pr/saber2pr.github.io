473eaff05d2df373648c6c70ad631cba02284005
saber2pr <saber2pr@gmail.com>
2021-05-22 15:33:39 +0800

    build(*): update

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,8 @@ export const get163Msg = async () => {
   const res = await axios.get<string>(ApiUrls.comments163)
   const text = res.data
   if (text) {
-    return text.slice(0, text.indexOf('来自@'))
+    const contentEnd = text.indexOf('来自@')
+    const anthEnd = text.indexOf('在「')
+    return text.slice(0, contentEnd) + '@' + text.slice(contentEnd + 3, anthEnd)
   }
 }
