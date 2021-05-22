26acfe7226b70aec6a1e633047804a245b1c6021
saber2pr <saber2pr@gmail.com>
2021-05-22 15:24:29 +0800

    feat: 图表添加跳转

--- src/modules/chart-pie/index.tsx
+++ src/modules/chart-pie/index.tsx
@@ -4,7 +4,7 @@ import React, { useMemo } from 'react'
 
 import { Loading } from '../../components'
 import { useEcharts } from '../../hooks'
-import { collect, TextTree } from '../../utils'
+import { collect, queryRootFirstChild, TextTree } from '../../utils'
 
 export interface ChartPie {
   title: string
@@ -13,9 +13,11 @@ export interface ChartPie {
 
 export const ChartPie = ({ data, title }: ChartPie) => {
   const ds = useMemo(() => collect(data), [data])
-  const pieData = useMemo(
-    () =>
-      ds.reduce((acc, item) => {
+  const [pieData, map] = useMemo(
+    () => {
+      const map: { [title: string]: TextTree } = {}
+      const result = ds.reduce((acc, item) => {
+        map[item.title] = item
         if (item.children) {
           return acc.concat({
             name: item.title,
@@ -23,41 +25,51 @@ export const ChartPie = ({ data, title }: ChartPie) => {
           })
         }
         return acc
-      }, []),
+      }, [])
+      return [result, map]
+    },
     [ds]
   )
 
   const [ref, loading] = useEcharts(
-    {
-      tooltip: {
-        trigger: 'item',
-        formatter(params: any) {
-          params = params[0] || params
-          return params.marker + params.name + '<br/>' + params.value + ' 篇'
-        },
-      },
-      grid: {
-        bottom: 30,
-        top: 30,
-        left: 30,
-        right: 30,
-      },
-      series: [
-        {
-          type: 'pie',
-          roseType: 'radius',
-          radius: ['40%', '70%'],
-          label: {
-            textBorderColor: 'transparent',
-            color: '#747474',
+    chart => {
+      chart.on('click', 'series.pie.label', (args: { data: { name: string } }) => {
+        if (map[args?.data?.name]) {
+          const firstChild = queryRootFirstChild(map[args?.data?.name])
+          location.hash = firstChild.path
+        }
+      })
+      chart.setOption({
+        tooltip: {
+          trigger: 'item',
+          formatter(params: any) {
+            params = params[0] || params
+            return params.marker + params.name + '<br/>' + params.value + ' 篇'
           },
-          itemStyle: {
-            borderRadius: 4,
-            borderWidth: 2,
-          },
-          data: pieData,
         },
-      ],
+        grid: {
+          bottom: 30,
+          top: 30,
+          left: 30,
+          right: 30,
+        },
+        series: [
+          {
+            type: 'pie',
+            roseType: 'radius',
+            radius: ['40%', '70%'],
+            label: {
+              textBorderColor: 'transparent',
+              color: '#747474',
+            },
+            itemStyle: {
+              borderRadius: 4,
+              borderWidth: 2,
+            },
+            data: pieData,
+          },
+        ],
+      })
     },
     [pieData]
   )
