96527e7c44085da70aa9d00a6fcaef2650e4563d
saber2pr <saber2pr@email.com>
2021-07-08 18:51:34 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,8 @@ const filterUrl = url =>
   url.includes('/api') ||
   url.includes('music.163.com') ||
   url.includes('music.126.net') ||
-  url.includes('saber2pr.top/editor')
+  url.includes('saber2pr.top/editor') || 
+  url.includes('cdn.jsdelivr.net')
 
 self.addEventListener('fetch', event => {
   const url = event.request.url
